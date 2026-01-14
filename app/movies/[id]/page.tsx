e807c3972413c9912c7140dfca8e6e1d9e502aab
BLUEHOLE\jaewoong <jaewoong@pubg.com>
2026-01-14 13:57:16 +0900

    🪄Enhance([id]/page.tsx, [id]/ui.tsx): 영화 상세 페이지 동적 데이터 및 메타데이터 추가
    영화 상세 페이지 개선
    - [id]/page.tsx: generateMetadata로 동적 SEO 메타데이터 생성
    - [id]/ui.tsx: 서버에서 받은 movie 데이터로 상세 정보 표시

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,11 +1,30 @@
+import { Metadata, ResolvingMetadata } from 'next';
 import UI from "./ui";
+import { GetMovieDetails } from "@/actions/movieActions";
 
-export default  async function MovieDetailPage({ params }: { params: Promise<{ id: string }> }) {
+export default  async function MovieDetailPage({ params }: { params: Promise<{ id: number }> }) {
     const { id } = await params;
+    const movie = await GetMovieDetails(id);
 
     return (
         <div className="absolute top-12 bottom-12 left-0 right-0 overflow-y-auto">     
-            <UI id={id} />            
+            <UI movie={movie} />            
         </div>
     )
+}
+
+export async function generateMetadata(
+    { params }: { params: Promise<{ id: number }> },
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+    const { id } = await params;
+    const movie = await GetMovieDetails(id);
+
+    return {
+        title: movie?.title,
+        description: movie?.overview,
+        openGraph: {
+            images: movie?.image_url
+        }
+    }
 }
--- app/movies/[id]/ui.tsx
+++ app/movies/[id]/ui.tsx
@@ -3,28 +3,28 @@
 import Image from "next/image";
 import { Star, UsersRound, CalendarDays } from "lucide-react";
 
-export default function UI({ id }) {    
+export default function UI({ movie }) {    
     return (
         <div className="flex w-full min-h-full items-center justify-center px-4 py-16 bg-foreground/90 text-background text-2xl md:flex-row flex-col gap-10">
             <Image
-                src='https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg'
+                src={movie.image_url}
                 alt="Movie Poster"
                 width={300}
                 height={450}
                 className="h-2/5 w-auto px-10 "
             />
             <div className="flex flex-col w-2/3 gap-8">
-                <h1 className="text-4xl font-bold">Dune: Part Two</h1>
+                <h1 className="text-4xl font-bold">{movie.title}</h1>
                 <p className="mt-4 text-md font-light opacity-75">
-                    Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.
+                    {movie.overview}
                 </p>
                 <div className="flex flex-col gap-2 text-md">
                     <p className="flex flex-row items-center gap-1">
-                        <Star/>Vote_average: 8.3</p>
+                        <Star/>Vote_average: {movie.vote_average}</p>
                     <p className="flex flex-row items-center gap-1">
-                        <UsersRound/>Popularity:3437.313</p>
+                        <UsersRound/>Popularity: {movie.popularity}</p>
                     <p className="flex flex-row items-center gap-1">
-                        <CalendarDays/>Release_date:2024-02-27</p>
+                        <CalendarDays/>Release_date: {movie.release_date}</p>
                 </div>
             </div>
         </div>
